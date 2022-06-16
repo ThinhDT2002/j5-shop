8d22752e7d54142f2280379af9c546db3bbdef36
ThinhDT2002 <85781083+ThinhDT2002@users.noreply.github.com>
2022-06-16 20:25:36 +0700

    Delete OrdersDetailRepository.java

--- SOF3021_PS15048_DoTienThinh_Assignment/src/main/java/com/assignment/service/OrdersDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assignment.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.assignment.model.OrdersDetail;
-
-public interface OrdersDetailRepository extends JpaRepository<OrdersDetail, String>{
-
-}
