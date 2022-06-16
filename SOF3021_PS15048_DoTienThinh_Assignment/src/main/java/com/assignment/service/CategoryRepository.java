2f654d27991c7d7e1a30b81dff4cf5b006b5386b
ThinhDT2002 <85781083+ThinhDT2002@users.noreply.github.com>
2022-06-16 20:24:46 +0700

    Delete CategoryRepository.java

--- SOF3021_PS15048_DoTienThinh_Assignment/src/main/java/com/assignment/service/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assignment.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.assignment.model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, String>{
-
-}
